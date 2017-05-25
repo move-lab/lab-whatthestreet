86d12f817f8330f8840fcdab83e520920b2f86a6
Thibault Durand <tibo.durand@gmail.com>
2017-05-25 12:45:53 +0300

    WIP: add other cities section

--- app/results/ResultsPage.js
+++ app/results/ResultsPage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Router from 'next/router';
 
+import OtherCities from './sections/OtherCities';
 import About from './sections/About';
 import Legal from './sections/Legal';
 import Footer from './sections/Footer';
@@ -11,6 +12,10 @@ class ResultsPage extends Component {
   render() {
     return (
       <div>
+        <OtherCities
+          city={this.props.city}
+          cities={this.props.availableCities}
+        />
         <About />
         <Legal cities={this.props.availableCities} />
         <Footer />
@@ -21,6 +26,7 @@ class ResultsPage extends Component {
 
 export default connect((state) => {
   return {
-    availableCities: state.city.get('availableCities').toJS()
+    availableCities: state.city.get('availableCities').toJS(),
+    city: state.city.get('actual_city').toJS()
   }
 })(ResultsPage)
---
+++ app/results/sections/OtherCities.js
@@ -0,0 +1,83 @@
+import React from 'react';
+
+import * as COLORS from '../../shared/style/colors';
+import * as METRICS from '../../shared/style/metrics';
+
+class OtherCities extends React.PureComponent {
+
+  static propTypes = {
+    cities: React.PropTypes.arrayOf(React.PropTypes.object),
+    city: React.PropTypes.object,
+  }
+
+  shouldComponentUpdate = () => (false)
+
+  renderCityLink = (city, index) => (
+    <li className="ListItem" key={index}>
+      <a href={`/${city.slug}`} className="Link">{city.name}</a>
+    </li>
+  )
+
+  render() {
+    return (
+      <div className="MainContainer" style={{ backgroundImage: `url(/api/v1/cities/${this.props.city.slug}/background)` }}>
+        <div className="Wrapper" style={{ backgroundImage: 'url("static/Background_Skyline@2x.jpg")' }}>
+          <h2 className="Title">
+            More Cities to discover
+          </h2>
+          <ul className="List">
+            {this.props.cities.map((city, index) => this.renderCityLink(city, index))}
+          </ul>
+        </div>
+        <style jsx>{`
+          .MainContainer {
+            color: white;
+            background-color: ${COLORS.OtherCitiesBackgroundColor};
+            background-repeat: no-repeat;
+            background-position: center center;
+            background-size: cover;
+          }
+
+          .Wrapper {
+            width: ${METRICS.MetricsContentWidth};
+            padding: ${METRICS.MetricsSectionPadding} ${METRICS.MetricsContentPadding};
+            margin: 0 auto;
+          }
+
+          .Title {
+            font-size: 34px;
+            font-weight: 400;
+            margin: 0 0 40px 0;
+          }
+
+          .List {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            align-items: flex-start;
+            justify-content: flex-start;
+          }
+
+          .Wrapper :global(.ListItem) {
+            flex-basis: 50%;
+            font-size: 47px;
+            line-height: 68px;
+          }
+
+          .Wrapper :global(.Link) {
+            cursor: pointer;
+            font-size: 47px;
+            line-height: 56px;
+            color: white;
+            text-decoration: none;
+          }
+        `}</style>
+      </div>
+    );
+  }
+}
+
+export default OtherCities;
