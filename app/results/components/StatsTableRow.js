62679ba1a185ba310a418c173f8626f0768ad632
Thibault Durand <tibo.durand@gmail.com>
2017-06-13 18:20:25 +0300

    Fix: formatting of Longest Streets table

--- app/results/components/StatsTableRow.js
+++ app/results/components/StatsTableRow.js
@@ -38,6 +38,7 @@ class StatsTableRow extends React.PureComponent {
             text-align: right;
             font-size: 24px;
             line-height: 51px;
+            width: 80px;
             font-weight: 200;
             font-family: 'LarsseitLight';
           }
