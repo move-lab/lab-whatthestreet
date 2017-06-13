0364eac23426e8fd465a6bb407cddcf04cb5b08e
Thibault Durand <tibo.durand@gmail.com>
2017-06-13 12:03:43 +0300

    WIP: display marker instead of polygon for small parking space

--- app/map/components/Map.js
+++ app/map/components/Map.js
@@ -158,6 +158,37 @@ class Map extends Component {
   renderParking(props) {
     const self = this;
     if(this.map) {
+
+      // Special case for small polygon (bike), display a marker
+      const firstCoordinate = props.parkingData.coordinates[0];
+      const differentValue = props.parkingData.coordinates.filter((value) => {
+        return value[0] !== firstCoordinate[0] && value[1] !== firstCoordinate[1]
+      });
+
+      // Display as marker
+      // TODO This is not working yet
+      if(differentValue.length === 0) {
+        const parkingFinal = {
+          type: 'Feature',
+          properties: {
+            "title": "...",
+            "marker-symbol": "marker"
+          },
+          geometry: {
+            type: 'Point',
+            coordinates: props.parkingData.coordinates[0],
+          }
+        }
+
+        this.map.getSource('data').setData(parkingFinal);
+        this.map.jumpTo({
+          center: props.parkingData.coordinates[0],
+          zoom: 18
+        });
+        return;
+      }
+
+
       // parking final geojson
       const parkingFinal = {
         type: 'Feature',
@@ -169,6 +200,7 @@ class Map extends Component {
           coordinates: [props.parkingData.coordinates],
         }
       }
+
       // initial parking geojson (like it is in the scroll experience)
       const parkingRotatedStart = rotate(parkingFinal, props.parkingData.rotation);
 
