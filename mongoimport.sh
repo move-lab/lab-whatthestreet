8c6c9dad8fb6eb067c4437f13648a30ad34bdca3
raphael <raphael.reimann@lab.moovel.com>
2017-06-16 18:35:12 +0200

    updated importscript because of download/unzipping problems

--- mongoimport.sh
+++ mongoimport.sh
@@ -2,7 +2,7 @@ cities=( "berlin" "boston" "johannesburg" "moscow" "stuttgart" "amsterdam" "buda
 
 for city in "${cities[@]}"
 do
-   unzip ${city}/coils\ MongoDB\ Dump/${city}_coiled.zip
+#   unzip ${city}/coils\ MongoDB\ Dump/${city}_coiled.zip
    mongorestore --drop -d ${city}_coiled ${city}_coiled/streets.bson
    mongorestore --drop -d ${city}_coiled ${city}_coiled/railtracks.bson
    mongorestore --drop -d ${city}_coiled ${city}_coiled/railtracksparking.bson
