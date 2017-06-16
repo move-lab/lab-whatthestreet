027d4470461b2810b035f0f05fb778d37f6b84a1
Thibault Durand <tibo.durand@gmail.com>
2017-06-16 15:35:26 +0300

    Evo: update cityMeta with 11 cities and add script to include space

---
+++ server/api/scriptAddSpaceToCityMeta.js
@@ -0,0 +1,23 @@
+var fs = require('fs');
+
+var srcPath = "cityMeta.json";
+
+fs.readFile(srcPath, 'utf8', function (err, cityMetaData) {
+    if (err) throw err;
+
+    let jsonData = JSON.parse(cityMetaData);
+
+   Object.keys(jsonData).map((cityKey) => {
+      var city = jsonData[cityKey];
+      jsonData[cityKey]["space"] = {
+        car: city.parking.car.offStreet.area + city.moving.car.area,
+        bike: city.parking.bike.area + city.moving.bike.area,
+        rail: city.parking.rail.area + city.moving.rail.area
+      }
+    })
+
+    fs.writeFile (srcPath, JSON.stringify(jsonData, null, 2), function(err) {
+        if (err) throw err;
+        console.log('complete');
+    });
+});
