aa16d3c1c4bdc4183e62b0c4458cd916fff8a902
Thibault Durand <tibo.durand@gmail.com>
2017-06-21 09:19:32 +0300

    Evo: add new cities array for raphael

--- mongoimport.sh
+++ mongoimport.sh
@@ -1,5 +1,6 @@
 cities=( "berlin" "boston" "johannesburg" "moscow" "stuttgart" "amsterdam" "budapest" "copenhagen" "rome" "singapore" "tokyo" )
-
+#new cities
+cities=( "barcelona" "beijing" "helsinki" "hongkong" "jakarta" "newyork" "portland" "sanfrancisco" "vienna" )
 for city in "${cities[@]}"
 do
 #   unzip ${city}/coils\ MongoDB\ Dump/${city}_coiled.zip
