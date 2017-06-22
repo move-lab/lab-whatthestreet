f3b05ce88940e210bbcdb1ec78795505b92fac9d
Thibault Durand <tibo.durand@gmail.com>
2017-06-22 18:25:28 +0300

    Evo: update svgs, still missing some

--- copysvg.sh
+++ copysvg.sh
@@ -1,14 +1,14 @@
-cities=( "berlin" "boston" "johannesburg" "moscow" "stuttgart" "amsterdam" "budapest" "copenhagen" "rome" "singapore" "tokyo" "barcelona" "beijing" "helsinki" "hongkong" "jakarta" "newyork" "portland" "sanfrancisco" "vienna" "chicago" )
-# cities=( "barcelona" "beijing" "helsinki" "hongkong" "jakarta" "newyork" "portland" "sanfrancisco" "vienna" )
+cities=( "berlin" "boston" "johannesburg" "moscow" "stuttgart" "amsterdam" "budapest" "copenhagen" "rome" "singapore" "tokyo" "barcelona" "beijing" "helsinki" "hongkong" "jakarta" "newyork" "portland" "sanfrancisco" "vienna" "chicago" "losangeles" )
+
 for city in "${cities[@]}"
 do
    mkdir -p ./static/cities/${city}/lanes
-   cp /Volumes/Data/TempMoovel/data/${city}/bike-tracks.svg ./static/cities/${city}/lanes/bike.svg
-   cp /Volumes/Data/TempMoovel/data/${city}/rail-tracks.svg ./static/cities/${city}/lanes/rail.svg
-   cp /Volumes/Data/TempMoovel/data/${city}/car-tracks.svg ./static/cities/${city}/lanes/car.svg
+   cp /Volumes/Data/TempMoovel/newdata/${city}/bike-tracks.svg ./static/cities/${city}/lanes/bike.svg
+   cp /Volumes/Data/TempMoovel/newdata/${city}/rail-tracks.svg ./static/cities/${city}/lanes/rail.svg
+   cp /Volumes/Data/TempMoovel/newdata/${city}/car-tracks.svg ./static/cities/${city}/lanes/car.svg
 
    mkdir -p ./static/cities/${city}/parking
-   cp /Volumes/Data/TempMoovel/data/${city}/bike-parking.svg ./static/cities/${city}/parking/bike.svg
-   cp /Volumes/Data/TempMoovel/data/${city}/rail-parking.svg ./static/cities/${city}/parking/rail.svg
-   cp /Volumes/Data/TempMoovel/data/${city}/car-parking.svg ./static/cities/${city}/parking/car.svg
+   cp /Volumes/Data/TempMoovel/newdata/${city}/bike-parking.svg ./static/cities/${city}/parking/bike.svg
+   cp /Volumes/Data/TempMoovel/newdata/${city}/rail-parking.svg ./static/cities/${city}/parking/rail.svg
+   cp /Volumes/Data/TempMoovel/newdata/${city}/car-parking.svg ./static/cities/${city}/parking/car.svg
 done
